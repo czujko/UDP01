5d29518a888e97a919a2cb4cd4ada9c44ffb7a78
czujko <55107723+czujko@users.noreply.github.com>
2022-01-13 09:00:35 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        InetAddress destino = InetAddress.getLocalHost();
+        int port = 9003;
+        byte[] mensaje = new byte[1024];
+
+        String mensaje_enviar = "Hola, soy el servidor";
+        mensaje = mensaje_enviar.getBytes();
+
+        DatagramPacket paquete = new DatagramPacket(mensaje, mensaje.length, destino, port);
+        DatagramSocket socket = new DatagramSocket(9999);
+
+        System.out.println("Enviando mensaje: " + mensaje_enviar);
+        System.out.println("Nombre: " + paquete.getAddress().getHostName());
+        System.out.println("Destino: " + destino.getHostAddress() + ":" + port);
+        System.out.println("Tamaño: " + paquete.getLength());
+        System.out.println("Paquete: " + paquete);
+
+
+        socket.send(paquete);
+        socket.close();
+    }
+}
